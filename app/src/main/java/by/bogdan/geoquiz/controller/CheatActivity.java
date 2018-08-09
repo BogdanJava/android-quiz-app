fae9d96f1f740051d5bbeaa96543aa103811d8fa
Bogdan Shishkin <bogdanshishkin1998@gmail.com>
2018-08-09 13:30:03 +0300

    final commit

--- app/src/main/java/by/bogdan/geoquiz/controller/CheatActivity.java
+++ app/src/main/java/by/bogdan/geoquiz/controller/CheatActivity.java
@@ -1,9 +1,14 @@
 package by.bogdan.geoquiz.controller;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AndroidRuntimeException;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +25,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private Question mQuestion;
     private TextView mAnswerTextView;
+    private TextView mApiLevelTextView;
     private Button mShowAnswerButton;
     private boolean isAnswerShown = false;
 
@@ -33,6 +39,7 @@ public class CheatActivity extends AppCompatActivity {
             throw new AndroidRuntimeException("Question extra shouldn't be null");
         this.mShowAnswerButton = findViewById(R.id.show_answer_button);
         this.mAnswerTextView = findViewById(R.id.answer_text_view);
+        this.mApiLevelTextView = findViewById(R.id.api_level_text_view);
 
         this.mShowAnswerButton.setOnClickListener(view -> {
             boolean answerTrue = mQuestion.isAnswerTrue();
@@ -43,8 +50,28 @@ public class CheatActivity extends AppCompatActivity {
             }
             isAnswerShown = true;
             setAnswerShownResult();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                final int cx = mShowAnswerButton.getWidth() / 2;
+                final int cy = mShowAnswerButton.getHeight() / 2;
+                float radius = mShowAnswerButton.getWidth();
+                final Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mAnswerTextView.setVisibility(View.VISIBLE);
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
+            } else {
+                mAnswerTextView.setVisibility(View.VISIBLE);
+                mShowAnswerButton.setVisibility(View.INVISIBLE);
+            }
         });
 
+        mApiLevelTextView.setText(getString(R.string.api_level, Build.VERSION.SDK_INT));
         if (savedInstanceState != null) {
             this.isAnswerShown = savedInstanceState.getBoolean(ANSWER_SHOWN_STATE, false);
         }
@@ -53,7 +80,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private void setAnswerShownResult() {
         Intent intent = new Intent();
-        intent.putExtra(EXTRA_ANSWER_SHOWN, this.isAnswerShown);
+        intent.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK, intent);
     }
 
